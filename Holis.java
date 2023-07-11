a26035c77a2b4ada769510471706909950c62390
EfrainMenchaca <82669165+EfrainMenchaca@users.noreply.github.com>
2023-07-11 16:41:35 -0600

    second change on github

--- Holis.java
+++ Holis.java
@@ -1,5 +1,5 @@
 public class Holis {
     public static void main(String args[]){
-        System.out.println("Hola hijo de la versh");
+        System.out.println("Hola hijo de la versh y holis bb Max :D");
     }
 }
